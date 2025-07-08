efd03a8d2cf4c891c430d2db424be4f7a6bd9b94
hoge1e3 <hoge1e3@gmail.com>
2025-07-08 11:49:25 +0900

    __unproxy__, peer in progress

--- src/proxy.ts
+++ src/proxy.ts
@@ -37,10 +37,39 @@ export function client(
     const c=new Client(target, channel, origin, manualProbe);
     return new Proxy(c,{
         get(target,p) {
+            if (p==="__unproxy__") {
+                return c;
+            }
             if (typeof p==="symbol" || p==="then" || p==="toString") {
                 return Reflect.get(target, p);
             }
             return (...args:any[])=>c.run(p,{args});
         }
     })
+}
+
+type PeerParamBase={
+    channel: string, 
+    service:{[key:string]: Function},      
+};
+type PeerParam=WorkerPeerParam|XOriginPeerParam;
+type WorkerPeerParam=PeerParamBase&{
+    target: Worker,
+};
+function isWorkerPeerParam(p:PeerParam):p is WorkerPeerParam {
+    return p.target instanceof Worker;
+}
+type XOriginPeerParam=PeerParamBase&{
+    target: Messagable,
+    targetOrigin: string,
+    allowOrigin?: string[],
+};
+export function peer(param:PeerParam) {
+    const [targetOrigin, allowOrigin]=isWorkerPeerParam(param)?
+    [undefined, [] as string[]]:
+    [param.targetOrigin, param.allowOrigin?? [param.targetOrigin]];
+    const {target, service, channel}=param;
+    const cli=client(target, channel, origin, false);
+    
+
 }
